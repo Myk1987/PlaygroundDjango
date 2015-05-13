807a2bddafa6278fefba1813a971dec2ee719836
Sergey Moukavoztchik <myk1987@gmail.com>
2015-05-13 13:00:25 +0300

    Add chnges to admin panel.

--- MyBar/bar/admin.py
+++ MyBar/bar/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Cocktail, Components, Ingredient
+
+admin.site.register(Cocktail)
+admin.site.register(Components)
+admin.site.register(Ingredient)
