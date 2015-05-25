93b14576524aed9b1c07027a768a5c90b2f3c3be
Sergey <myk1987@gmail.com>
2015-05-25 22:06:32 +0300

    Add first template

--- MyBar/bar/views.py
+++ MyBar/bar/views.py
@@ -2,5 +2,15 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from django.template import RequestContext, loader
+
+from .models import Cocktail
+
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the Cocktails index.")
+    cocktail_list = Cocktail.objects.order_by('name')
+    template = loader.get_template('bar/index.html')
+    context = RequestContext(request,
+                             {'cocktail_list': cocktail_list, }
+                             )
+    return HttpResponse(template.render(context))
