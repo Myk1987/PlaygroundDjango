38551f5b9f9edb9d72b49308ade7181ea3de0ec0
Sergey <myk1987@gmail.com>
2015-06-02 09:40:25 +0300

    urls update

--- MyBar/bar/urls.py
+++ MyBar/bar/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [url(r'^$', views.index, name='index'),
-               url(r'^(?P<cocktail_name>[A-Za-z]+)/$', views.cocktail_detail, name='cocktail_detail'), ]
+               url(r'^(?P<cocktail_name>\w+)/$', views.cocktail_detail, name='cocktail_detail'), ]
 # TODO: fix url for complex cocktail names with spaces and etc.
